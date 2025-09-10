1748c679e5b3a71e0c292e29889dfe2ce612c677
Juana Rodriguez <rodriguezjuana212@gmail.com>
2025-09-10 16:58:50 -0500

    Realice la sección de productos

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.slider-products').slick({
+    dots: false,
+    infinite: true,
+    arrows:true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                },
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+                },
+                {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            
+        ]
+    });
+});
