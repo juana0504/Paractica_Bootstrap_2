588639caecca1adb48416ae8d7ffa1933e404f0f
Juana Rodriguez <rodriguezjuana212@gmail.com>
2025-09-11 17:45:26 -0500

    Realice el responsive

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1
                 },
                 breakpoint: 768,
@@ -45,12 +45,12 @@ $(document).ready(function(){
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
                     slidesToScroll: 1
                 },
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                     slidesToScroll: 1
                 }
                 },
