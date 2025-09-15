ff91334afe7438e28fa09bbdb85e3038b122ea21
Juana Rodriguez <rodriguezjuana212@gmail.com>
2025-09-15 14:22:23 -0500

    Ultimas animaciones

--- js/main.js
+++ js/main.js
@@ -1,69 +1,75 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider-products').slick({
-    dots: false,
-    infinite: true,
-    arrows:true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-        responsive:[
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                },
-                breakpoint: 768,
+                breakpoint: 1080,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1
                 }
-                },
-                {
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
                 }
             }
-            
+
         ]
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.slider-reviews').slick({
-    dots: false,
-    infinite: true,
-    arrows:true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    lazyLoad: 'ondemand',
-        responsive:[
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        lazyLoad: 'ondemand',
+        responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1
-                },
+                }
+            },
+            {    
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
                 }
-                },
-                {
+            },
+            {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
                 }
             }
-            
+
         ]
     });
 });
+
+AOS.init();
